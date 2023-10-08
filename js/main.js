38fcea2f65c959aa8c91ad645199953f7b64dd3f
Grace <gracesilvaduarte@gmail.com>
2023-10-08 18:23:27 -0500

    jefes finales añadidos

--- js/main.js
+++ js/main.js
@@ -1,26 +1,18 @@
 /* selecciono el elemento carrusel */
 const carrousel = document.querySelector(".carrousel");
 
-/* deplazamiento al pasar el mouse
-const dragging = (e) =>{
-    carrousel.scrollLeft = e.pageX;
-}
-carrousel.addEventListener("mousemove", dragging); */
-
 /* ir hacia la izquierda y derecha */
 const rightButton = document.getElementById("rightButton");
 const leftButton = document.getElementById("leftButton");
 
+// botón izquierdo
 leftButton.onclick = function(){
     let card = document.querySelector(".carrousel").offsetWidth;
-  
     carrousel.scrollLeft -= card;
   }
   
   // botón derecho:
-  
   rightButton.onclick = function(){
     let card = document.querySelector(".carrousel").offsetWidth;
-  
     carrousel.scrollLeft += card;
   }
