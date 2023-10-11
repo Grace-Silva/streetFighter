7066265d347f5ff423fa22403a971a0a8cc0f943
Grace <gracesilvaduarte@gmail.com>
2023-10-10 19:26:49 -0500

    boton arriba

--- js/main.js
+++ js/main.js
@@ -23,13 +23,18 @@ goUp.addEventListener("click", () => {
   window.scrollTo(0, 0);
 });
 
+let header = document.querySelector("header");
+
+
 /* aparecer y desaparecer botón */
 window.onscroll = () => {
   if (window.scrollY <= 120) {
+    header.style.visibility="visible";
     goUp.style.visibility = "hidden";
     goUp.style.opacity = "0";
   } else {
     goUp.style.visibility = "visible";
     goUp.style.opacity = "1";
+    header.style.visibility="hidden";
   }
 };
